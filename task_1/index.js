3b26f40262cc80b5aab422f29932b7c3d3c6fbc5
Mikhail <Mikhail.24.Mitrofanov@yandex.ru>
2023-10-05 12:41:09 +0300

    task_1 COMPLITED

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,40 @@
 import {encoded, translations} from './data.js'
 
 console.log("Let's rock")
-console.log(encoded, translations)
+
+
+function decodeFields(encoded, translations) {
+    const decoded = [];
+    const uniqueIds = new Set();
+  
+    encoded.forEach((item) => {
+      const decodedItem = { ...item };
+  
+      for (const key in decodedItem) {
+        if (key.endsWith("Id") && !["groupId", "service", "formatSize", "ca"].includes(key)) {
+          const id = decodedItem[key];
+          if (id !== null && translations[id]) {
+            decodedItem[key] = translations[id];
+            uniqueIds.add(id);
+          }
+        }
+      }
+  
+      decoded.push(decodedItem);
+    });
+  
+    const uniqueIdList = Array.from(uniqueIds);
+  
+    return { decoded, uniqueIdList };
+  }
+  
+  const { decoded, uniqueIdList } = decodeFields(encoded, translations);
+  
+  console.log("Decoded obj:");
+  console.log(decoded);
+  console.log("Unique Ids:");
+  console.log(uniqueIdList);
 
 
 
-// console.log(decoded)
+
